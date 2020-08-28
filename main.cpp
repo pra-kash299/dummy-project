db0bc511e0388f411a0bc84176d754528d5eec12
pra-kash299 <69567752+pra-kash299@users.noreply.github.com>
2020-08-28 22:58:18 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{int i,j,k;
+cout<<"enter a number";
+cin>>i;
+for(k=1;k<=i;k++){
+for(j=1;j<=k;j++){
+    cout<<j;}
+   cout<<"\n";
+}
+    return 0;
+}
